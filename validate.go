fa4924f4216405c454822a070407b92b77e4fade
GabrielLLR <gabriel.ribeiro@sensedia.com>
2025-02-13 19:41:22 -0300

    Init

---
+++ validate.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"github.com/pb33f/libopenapi"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Uso: go run validate.go <arquivo-openapi.yaml>")
+		return
+	}
+
+	file := os.Args[1]
+	data, err := os.ReadFile(file)
+	if err != nil {
+		fmt.Println("Erro ao ler o arquivo:", err)
+		os.Exit(1)
+	}
+
+	doc, err := libopenapi.NewDocument(data)
+	if err != nil {
+		fmt.Println("Erro ao carregar OpenAPI:", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("✅ OpenAPI validado com sucesso!")
+}
